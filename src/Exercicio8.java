1fcc09261afbb2fd87b81a413cac838c987fab5c
carlinhoshk <carlosmdohk@gmail.com>
2022-04-26 16:50:04 -0300

    subindo exercicio feito em sala casa 8

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -2,8 +2,38 @@
 //brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total
 //de eleitores.
 
+// 10:38 - 26/04/2022
+//16:49 - 26/04/2022 fim
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Exercicio8 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numVotos, numBrancos, numNulos, numValidos ;
+
+
+
+        System.out.println("\nEscreva a Quantidade de Votos Nulos: ");
+        numNulos = sc.nextInt();
+
+        System.out.println("Escreva a Quantidade de Votos Brancos: ");
+        numBrancos = sc.nextInt();
+
+        System.out.println("Escreva a Quantidade de Votos Validos: ");
+        numValidos = sc.nextInt();
+
+        int total = numNulos + numValidos + numBrancos;
+
+        System.out.println("A Porcetagem de Votos Nulos é: " + (100*numNulos)/total + "%");
+        System.out.println("A Porcetagem de Votos Validos é: " + (100*numValidos)/total + "%");
+        System.out.println("A Porcetagem de Votos Brancos é: " + (100*numBrancos)/total + "%");
+
+
+
+        //percentual =
 
     }
 }
