2d58297796936594377887e388b97190b458bd59
carlinhoshk <carlosmdohk@gmail.com>
2022-05-20 22:17:50 -0300

    subindo exercicios feito em sala 20/05 :memo: :white_check_mark:

--- src/EstruturaFor/ExercicioH.java
+++ src/EstruturaFor/ExercicioH.java
@@ -1,5 +1,22 @@
-
+// 20:12 - 20/05/2022
+// Elaborar um programa que possibilite calcular a área total de uma residência (sala, cozinha,
+//banheiro, quartos, área de serviço, quintal, garagem, etc.). O programa deve solicitar a entrada do
+//nome, a largura e o comprimento de um determinado cômodo. Em seguida, deve apresentar a área
+//do cômodo lido e também uma mensagem solicitando do usuário a confirmação de continuar
+//calculando novos cômodos. Caso o usuário responda “NAO”, o programa deve apresentar o valor
+//total acumulado da área residencial.
 package EstruturaFor;
 
 public class ExercicioH {
+    public static void main(String[] args) {
+
+        int tamanhoSala, tamanhoCozinha, tamanhoBanheiro, TamanhoQuarto, TamanhoArea, TamanhoQuintal,
+                tamanhoGaragem;
+        String pergunta1 = "sim";
+        String pergunta = "não";
+
+        for (int i=1;i==1;i++){
+
+        }
+    }
 }
